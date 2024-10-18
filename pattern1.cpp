13cc27629bc3679719f8c204e43bdb0eff20a9a7
Sanjeevani Gupta <76159023+Sanjeevani09@users.noreply.github.com>
2024-10-18 23:40:56 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter number of lines of star : ";
+    cin>>n;
+
+    for(int i = 0; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+
+
+/*
+Pattern we want to print
+
+Enter number of lines of star : 5
+
+*
+**
+***
+****
+*****
+
+*/
