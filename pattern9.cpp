c5076cf86a34ca4a350fb12cbcc2f9ec96cc599e
Sanjeevani Gupta <76159023+Sanjeevani09@users.noreply.github.com>
2024-12-14 23:20:17 +0530

    Create pattern9.cpp

---
+++ pattern9.cpp
@@ -0,0 +1,19 @@
+
+
+
+
+
+// Pattern we want to print - Rhombus pattern
+
+// Enter any number : 5
+//     * * * * *
+//    * * * * *
+//   * * * * * 
+//  * * * * *
+// * * * * *
+
+// rows = 1 to n
+//2 loops = one for space and one for column
+//space = n to 1- row number
+// columns = 1 to row number
+// elements = row number + col number = even no then print 1 else 0
