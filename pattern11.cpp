8656db60c6db1be868f202afe5ede6d4b7e479c5
Sanjeevani Gupta <76159023+Sanjeevani09@users.noreply.github.com>
2024-12-15 09:45:20 +0530

    Create pattern11.cpp

---
+++ pattern11.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter any number you want : ";
+    cin>>n;
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = n; j >= i; j--){
+            cout<<" ";
+        }
+        for(int k = 1; k <= i; k++){
+            cout<<k<<" ";
+        }
+        
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+// Pattern we want to print - number Hill pattern
+// Enter any number you want : 5
+//      1
+//     1 2
+//   1 2 3 
+//   1 2 3 4
+//  1 2 3 4 5
+
+// rows = 1 to n
+// columns = 2 components (space and numbers)
+// spaces = 1 to n - row number
+// numbers = print row number
+// elements = numbers
+
+                            //OR
+
+
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter any number you want : ";
+    cin>>n;
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n - i; j++){
+            cout<<" ";
+        }
+        for(int k = 1; k <= i; k++){
+            cout<<k<<" ";
+        }
+        
+        cout<<endl;
+    }
+
+    return 0;
+}
