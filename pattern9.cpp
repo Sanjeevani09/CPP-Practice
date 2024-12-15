137e4aaafbceaf79d4bc05fdeb2b3870fe6d566d
Sanjeevani Gupta <76159023+Sanjeevani09@users.noreply.github.com>
2024-12-15 09:26:31 +0530

    Update pattern9.cpp

--- pattern9.cpp
+++ pattern9.cpp
@@ -31,3 +31,25 @@ int main() {
 //space = n to 1- row number
 // columns = 1 to row number
 // elements = row number + col number = even no then print 1 else 0
+
+           // OR
+
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter any number you want : ";
+    cin>>n;
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = i; j <= n; j++){
+            cout<<" ";
+        }
+        for(int k = 1; k <= n; k++){
+            cout<<"* ";
+        }
+        
+        cout<<endl;
+    }
+
+    return 0;
+}
