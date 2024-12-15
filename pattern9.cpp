605c7890a61e7a58c23b68eaeb33cc49888b11f0
Sanjeevani Gupta <76159023+Sanjeevani09@users.noreply.github.com>
2024-12-15 09:23:33 +0530

    Update pattern9.cpp

--- pattern9.cpp
+++ pattern9.cpp
@@ -1,7 +1,21 @@
-
-
-
-
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter any number you want : ";
+    cin>>n;
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = n; j >= i; j--){
+            cout<<" ";
+        }
+        for(int k = 1; k <= n; k++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
 
 // Pattern we want to print - Rhombus pattern
 
